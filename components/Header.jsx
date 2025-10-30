dd2195c40b6c0985a42cceee80194294e0a6de4d
Sudipofficial4 <officialsudip44@gmail.com>
2025-10-30 21:13:06 +0545

    changed the logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import Link from "next/link";
+
 
 import Socials from "../components/Socials";
 
@@ -7,18 +6,7 @@ const Header = () => {
   return (
     <header className="absolute z-30 w-full items-center px-16 xl-px-0 xl:h-[90px]">
       <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
-          {/* logo */}
-          <Link href="/">
-            <Image
-              src="/logo.svg"
-              alt="logo"
-              width={220}
-              height={48}
-              priority
-            />
-          </Link>
-
+        <div className="flex flex-col lg:flex-row justify-end items-center gap-y-6 py-8">
           {/* socials */}
           <Socials />
         </div>
